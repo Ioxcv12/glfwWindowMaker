7ed82b028273fba69856418f21b3a094c647ec9c
Ioxcv12 <160195069+Ioxcv12@users.noreply.github.com>
2025-06-02 00:48:49 -0700

    Update tris.cpp
    Added error detection and made it so that you can close the app by pressing escape

--- tris.cpp
+++ tris.cpp
@@ -3,9 +3,19 @@
 #include <GLFW/glfw3.h>
 #include <windows.h>
 
+auto err = std::make_error_code(std::errc::file_exists);
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 
-	glfwInit();
+	if (!glfwInit()) {
+		MessageBoxA(NULL, "ERROR", "GLFW failed to initialize.", MB_ICONERROR | MB_OK);
+		return -1;
+	}
 	gladLoadGL();
 
 	const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
@@ -18,6 +28,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
 
 	win = glfwCreateWindow(800, 600, "GLFW Test", NULL, NULL);
+
+	glfwSetKeyCallback(win, key_callback);
 
 	while (!glfwWindowShouldClose(win))
 	{
